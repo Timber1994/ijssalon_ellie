9fa7ae7de27f69f7ec130f9eefb4e982e6cb1347
Timber Groeneveld <timbergroeneveld@hotmail.com>
2023-03-20 16:02:47 +0100

    Fout aangepast in boekhouding.py

--- boekhouding.py
+++ boekhouding.py
@@ -16,6 +16,6 @@ presenteer(inkomsten, totaal_inkomsten)
 
 
 with open('boekhouding.csv', 'w',newline='') as csvfile:
-                for key, value in inkomsten.items():      
-                    writer = csv.writer(csvfile, delimter=';')      
-                    writer.writerow([key,value])
+    for key, value in inkomsten.items():
+        writer = csv.writer(csvfile, delimiter=';')
+        writer.writerow([key,value])
